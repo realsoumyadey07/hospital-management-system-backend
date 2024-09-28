aa228b49d328d5cc10f954b853cff02e882c3f70
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-09-28 14:27:25 +0530

    little issue solved

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,18 @@ import cors from "cors";
 
 export const app = express();
 
-app.use(express.json());
+app.use(express.json({
+     limit: "50mb"
+}));
+app.use(express.urlencoded({
+     extended: true,
+     limit : "50mb"
+}))
 app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.ORIGIN,
+    credentials: true
   })
 );
 
